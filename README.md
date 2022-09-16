7cc416a2f890ac7d92e9cf406ca3e72c81e8fbc6
luismaglz <luismaglz@live.com>
2022-09-16 13:47:33 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-sywqre
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-sywqre)
---
+++ index.ts
@@ -0,0 +1,17 @@
+// Import stylesheets
+import './style.css';
+import axios from 'axios';
+
+// Tasks
+
+// 1. Fetch data from IMDB API - link is provided - when AppComponent loads
+// 2. Save results from HTTP response in component state
+// 3. Display results in AppComponent template in table format displaying `title` and `description` fields in 2 columns
+// 4. Format title to be ALL CAPS
+// 5. Add some borders to the table
+// 6. (optional) Add input + button to make dynamic queries to IMDB API (searchKeyword)
+// 7. (optional) Display small image 50px wide in a new (first) column
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
