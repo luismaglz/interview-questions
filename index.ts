a0a864767eb4029918d3596f8bafed80bee86234
luismaglz <luismaglz@live.com>
2022-09-16 13:49:02 -0600

    1 file modified

--- index.ts
+++ index.ts
@@ -12,6 +12,19 @@ import axios from 'axios';
 // 6. (optional) Add input + button to make dynamic queries to IMDB API (searchKeyword)
 // 7. (optional) Display small image 50px wide in a new (first) column
 
+// endpoint
+const IMDB_API_SEARCH = 'https://imdb-api.com/en/API/SearchMovie/k_d3adyy0g/';
+
+// search keyword
+const searchKeyword = 'batman';
+
+// I movie interface returned by IMDB
+interface IMovie {
+  title: string;
+  description: string;
+  image: string;
+}
+
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
